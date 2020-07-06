a536f6e14b546b905416f78bce9cc623bcb91ebf
joshualondono <joshualondono@outlook.com>
2020-07-06 10:29:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,48 +3,158 @@
  ******************/
 
 
-const indexedChars = function(str) {
-  let result = '';
+function indexedChar(str){
+  let result = ''
   
-  for (let i = 0; i < str.length; i++){
-    result = result + i + str[i]
+  for(let i = 0; i < str.length; i++){
+    result += i + str[i]
   }
-  return result;
-
+  return result
 }
 
-const repeatIt = function(a,b){
+
+function exclaim(str){
+  let result = ''
   
-  let str = b
-  
-  for (let step = 0; step < a; step++) {
-  // Runs 5 times, with values of step 0 through 4.
-  console.log(str);
-}
-}
-
-repeatIt(4,'hi')
-
-
-
-function reverseString(str) {
-
-    let newString = "";
- 
-
-    for (let i = str.length - 1; i >= 0; i--) { 
-        newString += str[i]; // or newString = newString + str[i];
+  for(let i = 0; i < str.length; i++){
+    
+    if(str[i] === '?' || str[i] === '.'){
+     result = result + '!'
+    } else {
+      result = result + str[i]
     }
-   
-    return newString; // "olleh"
+   }
+  return result
 }
- 
-reverseString('hello');
+
+
+function repeat(str,val){
+  let result = ''
+  
+  for(let i = 0; i < str.length; i++){
+    result = result + str[i]
+  }
+  
+  for (let step = 0; step < val; step++) {
+  const output = result 
+  console.log(output);
+}
+}
+
+
+function repeatagain (str,max){
+   let result = ''
+   let count = 0 
+   
+   while (count < max){
+     result = result + str
+     count = count + 1
+   }
+return result 
+}
+
+function truncate(str){
+  
+  let result = ''
+  let rep_str = '...'
+  
+  for (let i = 0; i < 15; i++){
+    
+    if(i === 15 - 1){
+      result = result + rep_str
+    } else {
+      result = result + str[i]
+    }    
+  }  
+  return result
+}
+
+
+function ciEmailify(str){
+  let result = ''
+   
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === ' '){
+      result = result + '.'
+    } else if(i === str.length - 1) {
+      result = result + str[i] + '@codeimmersives.com'
+    } else {
+      result = result + str[i]
+    }
+  }
+  return result 
+}
+
+
+function reverse(str){
+  let result = ''
+  
+  for(let i = str.length - 1; i > -1; i--){
+    result = result + str[i] 
+  }
+  
+  return result 
+}
+
+
+function onlyVowels(str){
+  let result = ''
+  
+  for(let i = 0; i < str.length; i++){
+    
+    if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'){
+      result = result + str[i]
+    }
+  }
+  return result  
+}
 
 
 
+function crazyCase(str){
+  //every other value determined by even odd test
 
-indexedChars('healt')
+  let result = ''
+  let space = 0
+
+  for(let i = 0; i < str.length; i++){
+    
+
+       if (str[i] === ' '){
+       //everytime a space is found in string, space variable is incremented by 1 
+       result = result + str[i] 
+       space++
+         
+     } else if ([i + space] % 2 === 0) {
+       //space variable added to 
+       result = result + str[i].toLowerCase()
+     } else {
+       result = result + str[i].toUpperCase()
+     } 
+  } 
+  return result
+}
+
+function camelCase(str){
+  let result = ''
+  
+  for(let i = 0; i < str.length; i++){
+    
+   if(i === 0){
+     result = result + str[i].toLowerCase()
+   
+  } else if (str[i] === ' ' && str[i + 1] === ' '){
+    result = result
+  } else if (str[i] === ' ' && str[i + 1] != ' '){
+    result = result + str[i + 1].toUpperCase()
+    i++
+  } else {
+    result = result + str[i].toLowerCase()
+  }
+}
+    return result 
+
+}
 
 
 
